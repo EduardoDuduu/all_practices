57e749cd112080c2fdadd5a15ae71c79170285f5
Eduardo de Oliveira Marques <98243709+EduardoDuduu@users.noreply.github.com>
2022-12-29 01:33:36 -0300

    Update script1.js

--- curso_js/exercicios/ex013/script1.js
+++ curso_js/exercicios/ex013/script1.js
@@ -1,9 +1,9 @@
 var cont = 1
 
-while (cont <= 10) {
+do {
     console.log(`Olá mundo! ${cont}`)
     cont++ //cont = cont + 1
-}
+} while (cont <= 10)
 
 
 
@@ -11,6 +11,16 @@ while (cont <= 10) {
 
 
 /*
+
+var cont = 1
+
+while (cont <= 10) {
+    console.log(`Olá mundo! ${cont}`)
+    cont++ //cont = cont + 1
+}
+
+--------------------------------
+
 console.log('Olá mundo!')
 console.log('Olá mundo!')
 console.log('Olá mundo!')
