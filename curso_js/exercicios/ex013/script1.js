579722056177b96060326134ee53a94387cb0dfb
Eduardo de Oliveira Marques <98243709+EduardoDuduu@users.noreply.github.com>
2022-12-29 01:29:46 -0300

    Update nos exercícios de javascript
    Estruturas de repetição!

---
+++ curso_js/exercicios/ex013/script1.js
@@ -0,0 +1,29 @@
+var cont = 1
+
+while (cont <= 10) {
+    console.log(`Olá mundo! ${cont}`)
+    cont++ //cont = cont + 1
+}
+
+
+
+
+
+
+/*
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+*/
