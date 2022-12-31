5fd4c8c0846cd2f4c02b5a23ec2195be1b418442
Eduardo de Oliveira Marques <98243709+EduardoDuduu@users.noreply.github.com>
2022-12-31 01:28:10 -0300

    update site

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,7 @@ function clicouDesafio() {
     if (window.document.getElementById('conteudoLink1').style.display == 'none') {
         var setaMostra1 = window.document.getElementById('mostrarContent1').style.display = 'inline'
         var setaEsconder1 = window.document.getElementById('esconderContent1').style.display = 'none'
-        var subTitle1 = window.document.getElementById('subTitulo1').style.display = 'inline'
+        var subTitle1 = window.document.getElementById('subTitulo1').style.display = 'block'
         var contLink1 = window.document.getElementById('conteudoLink1').style.display = 'flex'
     } else if (window.document.getElementById('conteudoLink1').style.display == 'flex') {
         var setaMostra1 = window.document.getElementById('mostrarContent1').style.display = 'none'
@@ -33,7 +33,7 @@ function clicouExercicio() {
     if (window.document.getElementById('conteudoLink2').style.display == 'none') {
         var setaMostra2 = window.document.getElementById('mostrarContent2').style.display = 'inline'
         var setaEsconder2 = window.document.getElementById('esconderContent2').style.display = 'none'
-        var subTitle2 = window.document.getElementById('subTitulo2').style.display = 'inline'
+        var subTitle2 = window.document.getElementById('subTitulo2').style.display = 'block'
         var contLink2 = window.document.getElementById('conteudoLink2').style.display = 'flex'
     } else if (window.document.getElementById('conteudoLink2').style.display == 'flex') {
         var setaMostra2 = window.document.getElementById('mostrarContent2').style.display = 'none'
