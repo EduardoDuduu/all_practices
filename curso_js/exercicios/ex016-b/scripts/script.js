8bb8b5f071449d0effe29dfb17435cbb05dbe8ea
Eduardo de Oliveira Marques <98243709+EduardoDuduu@users.noreply.github.com>
2023-01-05 02:41:21 -0300

    Criei um analisador de números

---
+++ curso_js/exercicios/ex016-b/scripts/script.js
@@ -0,0 +1,62 @@
+let num = document.querySelector('input#itxtnum')
+let lista = document.querySelector('select#iselnum')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n <= 100)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = window.document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        alert('Valor inválido ou já existente na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O maior número é ${maior}</p>`
+        res.innerHTML += `<p>O menor número é ${menor}</p>`
+        res.innerHTML += `<p>A soma de todos os valore é ${soma}</p>`
+        res.innerHTML += `<p>A média de todos os valores é ${media}</p>`
+    }
+}
